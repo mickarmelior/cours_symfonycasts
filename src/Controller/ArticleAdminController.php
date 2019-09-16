b30e0ee6d5b7c2d426b5c73977550e2bc02d7acd
armelior <mickbo@laposte.net>
2019-09-16 08:16:12 +0200

    forms chap 5

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleFormType;
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleAdminController extends AbstractController
 {
@@ -16,22 +18,50 @@ class ArticleAdminController extends AbstractController
      * @Route("/admin/article/new", name="admin_article_new")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $em, Request $request)
     {
-        die('todo');
+        $form = $this->createForm(ArticleFormType::class);
 
-        return new Response(sprintf(
-            'Hiya! New Article id: #%d slug: %s',
-            $article->getId(),
-            $article->getSlug()
-        ));
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Article $article */
+            $data = $form->getData();
+            dd($data);
+            $article = new Article();
+            $article->setTitle($data['title']);
+            $article->setContent($data['content']);
+            $article->setAuthor($this->getUser());
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article Created! Knowledge is power!');
+
+            return $this->redirectToRoute('admin_article_list');
+        }
+
+        return $this->render('article_admin/new.html.twig', [
+            'articleForm' => $form->createView()
+        ]);
     }
 
     /**
      * @Route("/admin/article/{id}/edit")
      * @isGranted("MANAGE", subject="article")
      */
-    public function edit(Article $article){
+    public function edit(Article $article)
+    {
         dd($article);
+    }
+
+    /**
+     * @Route("/admin/article", name="admin_article_list")
+     */
+    public function list(ArticleRepository $articleRepo)
+    {
+        $articles = $articleRepo->findAll();
+        return $this->render('article_admin/list.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 }
